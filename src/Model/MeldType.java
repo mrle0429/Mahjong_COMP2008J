21f76dd0c6bbbf7964234e679af7749e6c412ae9
刘乐 <120909827+mrle0429@users.noreply.github.com>
2024-05-26 00:02:34 +0800

    Revert "Game UI"

--- src/Model/MeldType.java
+++ src/Model/MeldType.java
@@ -4,6 +4,5 @@ public enum MeldType {
     PENG, // For 3 same tiles
     GANG, // For 4 same tiles
     EAT, // For 3 in a sequence
-    ANGANG, // For 4 same tiles in hand, not shown to other players
     WIN;
 }
--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,16 +1,12 @@
 package Model;
 
-import java.util.List;
-
 public class Player {
     private PlayerType location;
     private Hand hand;
     private boolean isWinner;
     private boolean isZhuang;
 
-
     private int score;
-
 
 
     public Player(PlayerType location) {
@@ -21,52 +17,39 @@ public class Player {
         score = 500;
     }
 
-
-    public boolean drawTile(TileStack tileStack) {  // 抓牌
-        boolean hasDrawn = hand.drawTile(tileStack);
-        if (hand.checkIsWin()) {
-            isWinner = true;
+    public void drawTile(TileStack tileStack){  // 抓牌
+        Tile tile = tileStack.takeTile();
+        if (tile != null) {
+            hand.addTile(tile);
+            if (hand.checkIsWin()) {
+                isWinner = true;
+            }
         }
-        return hasDrawn;
     }
 
-    public boolean discardTile(TileStack tileStack, Tile tile) {
+    public boolean discardTile(TileStack tileStack, String tileName){
+        if(!hand.discardTile(tileStack, tileName)){
+            System.out.println("You just can discard tile that on your hand");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean discardTile(TileStack tileStack, Tile tile){
         return hand.discardTile(tileStack, tile);
     }
 
-    public void pengTile(Tile tile) {
+    public void showHandTiles(){
+        hand.showHandTiles();
+    }
+
+    public void peng(Tile tile) {
         hand.operation(MeldType.PENG, tile);
     }
 
-    public void gangTile(Tile tile) {
+    public void gang(Tile tile) {
         hand.operation(MeldType.GANG, tile);
     }
-
-    public void eatTile(Tile tile) {
-        hand.operation(MeldType.EAT, tile);
-    }
-
-    public void anGangTile() {
-        hand.operation(MeldType.ANGANG, null);
-    }
-
-    public boolean checkPung(Tile tile) {
-        return hand.canPeng(tile);
-    }
-
-    public boolean checkGang(Tile tile) {
-        return hand.canGang(tile);
-    }
-
-    public boolean checkEat(Tile tile) {
-        return hand.canEat(tile);
-    }
-
-    public boolean checkAnGang() {
-        return hand.canAnGang();
-    }
-
-
 
     public boolean isWinner() {
         return isWinner;
@@ -105,17 +88,5 @@ public class Player {
     @Override
     public String toString() {
         return this.location.toString();
-    }
-
-    public boolean firstDrawFinish() {
-        return hand.isDealingFinished();
-    }
-
-    public List<Tile> getTiles() {
-        return hand.getTiles();
-    }
-
-    public List<Tile> getMeldTiles() {
-        return hand.getMeldTiles();
     }
 }
